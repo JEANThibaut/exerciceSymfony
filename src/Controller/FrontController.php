21684dd2d67805b8535d3e690b7e070402760c99
Thibaut <thibaut-jean@live.fr>
2021-06-25 15:13:40 +0200

    add article and addArticle route

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    #[Route('/front', name: 'front')]
+    public function index(): Response
+    {
+        return $this->render('front/index.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+
+    #[Route('/article/{id}', name:'single')]
+    public function article(int $id = 1): Response
+    {   
+        return $this->render('front/article.html.twig', [
+            'id'=>$id,
+            'controller_name' => 'FrontController',
+        ]);
+    }
+
+    #[Route('/admin/article/add', name:'addArticle')]
+    public function addArticle(): Response
+    {
+        return $this->render('front/addArticle.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+
+}

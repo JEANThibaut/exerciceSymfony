7f8b64fe047d8a62840510e0664ca9478288c80c
Thibaut <thibaut-jean@live.fr>
2021-06-29 22:08:27 +0200

    pass article to index View from DB

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,9 +12,12 @@ class FrontController extends AbstractController
     #[Route('/', name: 'index')]
     #[Route('/home', name: 'home')]
     public function index(): Response
-    {
+    {   
+        $articleRepository=$this->getDoctrine()->getRepository(Article::class);
+        $articles=$articleRepository->findAll();
+        
         return $this->render('front/index.html.twig', [
-            'controller_name' => 'FrontController',
+            'articles' => $articles,
         ]);
     }
 

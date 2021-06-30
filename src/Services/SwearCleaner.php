0f6219440cd2151bf3d724e0d5fe6c061d81757b
Jean Thibaut <thibaut-jean@live.fr>
2021-06-30 16:54:50 +0200

    prepare clearSwear

---
+++ src/Services/SwearCleaner.php
@@ -0,0 +1,15 @@
+<?php
+
+
+ class SwearCleaner{
+
+
+    public function clearSwear(Article $article): Response{
+               
+        // $offend=[];
+        
+            return
+    }
+ }
+
+?> 
